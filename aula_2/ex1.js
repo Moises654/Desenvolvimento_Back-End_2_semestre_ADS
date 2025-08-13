7ed7122bb72d1cf5414f78bce1dce4032dcc1603
Moises654 <moisescardoso654@gmail.com>
2025-08-13 20:31:46 -0300

    Aula 2
    Exemplo "Hello, world !" Java Script

---
+++ aula_2/ex1.js
@@ -0,0 +1,7 @@
+// Primeiro exemplo de código em Java Script
+
+const idade = 29; // declaro uma constante chamada idade
+const nome = "Moisés"; // declara constante chamada nome
+
+// Imprime uma mensagem no terminal
+console.log("Olá, Moisés !");
