087429d265f102487ea3822752e8cb3259cc7ba5
Moises654 <moisescardoso654@gmail.com>
2025-08-13 21:38:32 -0300

    Aula 2
    Verificando e exibindo o tipo da variável.

--- aula_2/ex1.js
+++ aula_2/ex1.js
@@ -2,6 +2,12 @@
 
 const idade = 24; // declaro uma constante chamada idade
 const nome = "Moisés"; // declara constante chamada nome
+const inf = nome + " " + idade; // junta o nome com a idade
 
 // Imprime uma mensagem no terminal
 console.log("Olá, Moisés !");
+
+// Exibindo o valor das constantes no terminal
+
+console.log(`${nome} idade ${idade}`); // exibe o valor no terminal
+console.log(inf); // 
