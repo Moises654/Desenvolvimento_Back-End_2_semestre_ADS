8429aaf03d7fe63e562e901cd0ec045d41876736
Moises654 <moisescardoso654@gmail.com>
2025-09-24 20:39:16 -0300

    Aula 4 e 5
    Requisição GET utilizando a API Open Weather; Endpoint/ multiple; Endpoint/ alert.

--- Aula 4/server.js
+++ Aula 4/server.js
@@ -1,3 +1,35 @@
+// =======================
+// IMPORTA DEPENDÊNCIAS
+// =======================
+const express = require ('express'); // Framework para criar servidor web
+const cors = require ('cors'); // Permite requisições externas (CORS)
+const axios = require ('axios'); // Para fazer requisições HTTP a APIs externas
+
+// =======================
+// CONFIGURAÇÃO DO SERVIDOR
+// =======================
+const app = express(); // Cria a instância do servidor Express
+const PORT = 3000; // Define a porta do servidor
+const apiKey = 'ca889b600944645ab4be9db1d2cd7d99'; //Sua chave da API OpenWeatherMap
+
+// Habilita CORS, permitindo que qualquer site acesse a API
+app.use(cors());
+
+//========================
+// ROTA GET /weather
+// =======================
+/*
+app.get ('/weather', async (req, res) => {
+    // Captura os parâmetros da URL: ?city=Lisboa&country=PT
+    const {city, country} = req.query;
+
+    // Validação: se cidade ou país não forem informados, retorna erro 400
+    if (!city || !country) {
+        return res.status(400).json({error: 'Informe cidade e país'});
+    }
+})
+*/    
+
 // ===========================
 // ROTA GET /alert
 // ===========================
@@ -30,4 +62,11 @@ app.get ('/alert', async (req, res) => {
         // Retorna erro caso falha a requisição
         res.status(500).json({error: 'Erro ao obter dados do clima.'});
     }
+});
+
+// ========================
+// INICIA O SERVIDOR
+// ========================
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
