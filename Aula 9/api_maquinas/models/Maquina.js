6cd6a4e5830ca11f8636426ee07b91419878fb06
Moises654 <moisescardoso654@gmail.com>
2025-10-01 19:54:43 -0300

    Aula 9
    Criação API Maquinas
    Cria models máquina

---
+++ Aula 9/api_maquinas/models/Maquina.js
@@ -0,0 +1,15 @@
+// importa biblioteca do mongoose
+const mongoose = require ("mongoose");
+
+// Cria a estrutura (schema) para b documento da maquina
+const MaquinaSchema = new mongoose.Schema({
+    nome:{type: String, required: true}, // Nome da máquina
+    tipo: {type: String, required: true}, // Tipo ex impressora, rotativa
+    status:{type: String, required: true}, // status ativa, inativa, manutenção
+    ultimaManutencao: {type: Date, required: true}, // última manutenção
+    proximaManutencao: {type: Date, required: True} // próxima manutenção
+
+})
+
+// Exporta o modelo
+module.exports = mongoose.model ("Maquina", MaquinaSchema);
