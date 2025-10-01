3d3b586983165d149269be38d6e899586feb2d50
Moises654 <moisescardoso654@gmail.com>
2025-10-01 20:54:21 -0300

    Aula 9
    CRUD para o usuário
    Implementação das rotas

---
+++ Aula 9/api_maquinas/routes/usuario.js
@@ -0,0 +1,49 @@
+// Cria os mátodos post, get e delete para os usuários
+// importa as bibliotecas
+const express = require ("express"); // biblioteca express
+const router = express.Router(); // biblioteca router
+const Usuario = require ("../models/Usuario") // importa o usuário a partir do models 
+
+// Cria usuario
+ router.post("/",async(req,res)=>{
+    try{
+        const usuario = new Usuario(req.body);
+        const salvo = await usuario.save(); // salva usuario
+        res.status(201).json(salvo);
+    }catch(err){
+        res.status(400).json({error:err.message});
+    }
+
+ });
+
+ // Lista todos os usuários
+router.get("/", async (req,res)=>{
+    try{
+        const usuarios = await Usuario.find();
+        res.json (usuarios);
+    }catch(err){
+        res.status (500).json ({error: err.message});
+    }
+});
+
+// Atualiza usuário por ID
+router.put ("/:id", async (req,res)=>{
+    try{
+        const atualizado = await Usuario.findByIdAndUpdate (req.params.id,req.body, {new: true});
+    }catch(err){
+        res.status(400).json({error:err.message});
+    }
+});
+
+// Deleta usuário por ID
+router.delete("/:id",async(req,res)=>{
+    try{
+        await Usuario.findByIdAndDelete(req.params.id);
+        res.json({message:"Usuário deletado com sucesso"});
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+
+ });
+
+ module.exports = router;
