f9cf5499efd421c9f765b4dc31704499ab45ddbc
Moises654 <moisescardoso654@gmail.com>
2025-10-01 20:38:17 -0300

    Aula 9
    Cria os métodos http POST, PUT, GET e DELETE para a rota das máquinas

---
+++ Aula 9/api_maquinas/routes/maquina.js
@@ -0,0 +1,70 @@
+// Cria os métodos http para cadastrar, atualizar, deletar e listar as máquinas cadastradas
+
+// Importa as bibliotecas
+const express = require ("express"); // importa biblioteca express
+const mongoose = require ("mongoose"); // importa biblioteca mongoose
+const Maquina = require ("../models/Maquina"); // importa a maquina da pasta models
+
+const router = express.Router (); // pega a rota a partir da biblioteca Express
+
+// Cria o método post
+router.post ("/", async (req,res)=>{
+    try {
+        const {nome, tipo, status, ultimaManutencao, proximaManutencao}=req.body; // armazena o corpo da requisição
+        const novaMaquina = await Maquina.create ({nome, tipo, status})
+        res.status (201).json (novaMaquina);
+    }catch(err){
+        res.status(400).json({error:err.message});
+    }
+
+});
+
+// Cria o métodoget all - lista todas as máquinas cadastradas
+
+router.get("/",async(req,res)=>{
+    try{
+        const maquinas= await Maquina.find();
+        res.json(maquinas);
+
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+// Cria o método get one - lista uma máquina especifica pelo id
+
+router.get ("/:id", async(req, res)=>{
+    try{
+        const maquina = await Maquina.findBy(req.params.id);
+        if(!maquina) return res.status(404).json({mensagem: "Máquina não encontrada"});
+        res.json (maquina);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+// Cria o método put para modificar uma máquina existente
+router.put("/:id",async(req,res)=>{
+    try{
+        const {nome,tipo,status,ultimaManutencao,proximaManutencao} = req.body;
+        const maquinaAtualizada = await Maquina.findByIdAndUpdate(
+            req.params.id,{nome,tipo,status,ultimaManutencao,proximaManutencao},{new:true}
+        );
+        if(!maquinaAtualizada) return res.status(404).json({message:"Máquina não encontrada"});
+        res.json(maquinaAtualizada);
+    }catch(err){
+        res.status(404).json({error: err.message});
+    }
+});
+
+// Cria o método delete
+router.delete ("/:id", async(req,res)=>{
+    try{
+        const maquinaDeletada = await Maquina.findByIdAndDelete(req.params.id);
+        if(!maquinaDeletada) return res.status(404).json({mensagem: "Máquina não encontrada"});
+    } catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+module.exports = router;
