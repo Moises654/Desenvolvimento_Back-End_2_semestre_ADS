3f9a2a4f18b507440732b0dad3ee8c49663e552a
Moises654 <moisescardoso654@gmail.com>
2025-10-01 21:43:12 -0300

    Aula 9
    Server.js

---
+++ Aula 9/api_maquinas/server.js
@@ -0,0 +1,29 @@
+// arquivo que cria nossa api
+// importa as bibliotecas
+const express = require("express"); // importa a biblioteca express
+const mongoose = require(mongoose); // importa a biblioteca mongoose
+
+const usuarioRoutes = require("./routes/usuario"); // importa a rota do usuário
+const maquinaRoutes = require("./routes/maquina"); // importa a rota maquina
+
+const app = express(); // cria a variável app para armazenar o express
+const PORTS = 4000;
+
+// Middlewarepara interpretar o json
+app.use(express.json());
+
+// Conecta ao Mongo DB
+mongoose.connect("mondbd://localhost:27017db_api", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+
+}).then(()=>console.log("Conectado ao Mongo DB")).catch(err=>console.error("Erro ao conectar", err));
+
+// Usando as rotas de usuário e máquina
+app.use("/api/usuario",usuarioRoutes);
+app.use("/api/maquina",maquinaRoutes);
+
+// Inicia o servidor
+app.listen(PORTS,()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
