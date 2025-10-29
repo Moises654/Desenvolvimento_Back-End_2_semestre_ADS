cb95ecf6c092047ffcaaf97697c26a70b0e5a12a
Moises654 <moisescardoso654@gmail.com>
2025-10-29 20:24:23 -0300

    Aula 13
    JWT API
    Middleware
    auth.js

---
+++ Aula 13/jwt-api/middleware/auth.js
@@ -0,0 +1,22 @@
+// código de autenticação de middleware
+
+const jwt = require ("jsonwebtoker"); // importa a biblioteca jsonwebtoken
+
+// cria a função para realizar a autenticação
+
+function auth (req, res, next){
+    const authHeader = req.header.authorization || "";
+    const token = authHerder.startWith ("Bearer ") ? authHeader.slice (7): null;
+    
+    if (!token) return res.status (401).json ({error: "Token ausente"});
+
+    try {
+        const payload = jwt.verify (token, process.env.JWT_SECRET);
+        req.user = {id: payload.sub, email: payload.email, name: payload.name};
+        next ();
+    }catch (err){
+        return res.status (401).json ({error: "Token inválido ou expirado"});
+    }
+}
+
+module.exports = auth; // exporta o auth
